e604700fe25aa6c59a3b51eaae8fc65dadc0904f
Eduardo Santiago <esantiago.coder@gmail.com>
2023-01-11 00:37:29 -0600

    R key to Restart Level

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField]
+    private bool _isGameOver;
+
+    private void Update()
+    {
+        //if the R key was pressed
+        //restart the current scene
+        if (Input.GetKeyDown(KeyCode.R) && _isGameOver == true)
+        {
+            SceneManager.LoadScene(0); //Current Game Scene
+            //Para asignar la escena 0 primero debemos ir a File -> Build Settings -> Add scene y se generará un indice para la escena en cuestión
+        }
+    }
+
+    public void GameOver()
+    {
+        Debug.Log("GameManager::GameOver() Called");
+        //en el comentario profesionalmente indicamos la clase::metodo
+        _isGameOver = true;
+    }
+
+}
--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -15,6 +15,11 @@ public class UIManager : MonoBehaviour
     private Sprite[] _liveSprites;
     [SerializeField]
     private Text _gameOverText;
+    [SerializeField]
+    private Text _restartText;
+
+    private GameManager _gameManager;
+
 
 
     void Start()
@@ -22,6 +27,13 @@ public class UIManager : MonoBehaviour
         //assign text component to the handle
         _scoreText.text = "Score: " + 0;
         _gameOverText.gameObject.SetActive(false);
+        _gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+
+        //Es importante saber si la referenicia que buscamos de nuestro GameObject es nulo o no
+        if (_gameManager == null)
+        {
+            Debug.Log("GameManager is NULL.");
+        }
     }
 
 
@@ -39,9 +51,20 @@ public class UIManager : MonoBehaviour
 
         if(currentLives == 0)
         {
-            _gameOverText.gameObject.SetActive(true);
-            StartCoroutine(GameOverFLickerRoutine());
+            GameOverSequence();
         }
+    }
+
+    void GameOverSequence()
+    {
+        _gameManager.GameOver();
+        _gameOverText.gameObject.SetActive(true);
+        _restartText.gameObject.SetActive(true);
+        //tiene sentido colocar la lectura de la tecla R aqui? no, para eso se diseña un GAME MANAGER, que es el responsable de gestionar el estado del juego:
+        //Se ha acabado la partida? el jugador sigue vivo?, Cuantos enemigos hay en el juego?
+        //Al Crear el Script Game Manager Unity le coloca un icono de Egranaje, es una configuragion por default
+
+        StartCoroutine(GameOverFLickerRoutine());
     }
 
     IEnumerator GameOverFLickerRoutine()
